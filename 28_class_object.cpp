c38fb155bce33af2c129dabe826ac625be82a629
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-13 10:29:17 +0530

    07/07

--- 28_class_object.cpp
+++ 28_class_object.cpp
@@ -8,32 +8,33 @@ class mouse
     float rating;
 
 public:
-    void setMouse(string c, string co, int p, float r)
+    void setMouse(string c, string co, int p, float r) // setter method
     {
         company = c;
         color = co;
         price = p;
         rating = r;
     }
-    void getMouse()
+    void getMouse() // getter method
     {
         cout << "\nmouse info : " << endl;
         cout << "company : " << company << endl;
         cout << "color : " << color << endl;
         cout << "price : " << price << endl;
         cout << "rating : " << rating << endl;
+        cout << "--------------------------" << endl;
     }
 };
 int main()
 {
-    mouse m1, m2, m3;
+    mouse m1, m2, m3, m4;
     m1.setMouse("hp", "red", 300, 3.5);
     m2.setMouse("dell", "black", 500, 4.5);
     m3.setMouse("lenova", "white", 700, 2.6);
-
+    m4.setMouse("frontech", "pink", 700, 1.7);
     m1.getMouse();
     m2.getMouse();
     m3.getMouse();
-
+    m4.getMouse();
     return 0;
 }
---
+++ 29_student.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class student
+{
+    string name;
+    int rno;
+    float per;
+
+public:
+    void setStudent(string n, int r, float p)
+    {
+        name = n;
+        rno = r;
+        per = p;
+    }
+
+    void getResultCard()
+    {
+        cout << "student result card ....." << endl;
+        cout << "student name : " << name << endl;
+        cout << "student rno : " << rno << endl;
+        cout << "student per : " << per << endl;
+        if (per >= 33)
+        {
+            cout << "student pass" << endl;
+        }
+        else
+        {
+            cout << "student fail" << endl;
+        }
+        cout << "---------------------" << endl;
+    }
+};
+int main()
+{
+    student s1, s2, s3;
+    s1.setStudent("ram sharma", 101, 56.78);
+    s2.setStudent("vansh patidar", 102, 30);
+    s3.setStudent("virat kohli", 103, 90);
+    s1.getResultCard();
+    s2.getResultCard();
+    s3.getResultCard();
+    return 0;
+}
